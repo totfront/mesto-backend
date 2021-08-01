79e63b76e0e05fb25edfd1f4f86f990f20729723
Alex Mir <totfront@gmail.com>
2021-08-01 14:18:56 +0300

    feat: any invalid route handler

--- app.js
+++ app.js
@@ -23,8 +23,13 @@ app.use((req, res, next) => {
 });
 
 app.use("/users", require("./routes/users"));
-
 app.use("/cards", require("./routes/cards"));
+
+app.use("*", (req, res) => {
+  res
+    .status(404)
+    .send(JSON.stringify({ message: "Запрашиваемый ресурс не найден" }));
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
