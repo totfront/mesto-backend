715fe0bb0ab6c56c594b10cbc239684ab9ece4c2
Alex Mir <totfront@gmail.com>
2021-08-01 20:14:39 +0300

    feat: helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
 
 const { PORT = 3000, BASE_PATH } = process.env;
 
@@ -15,6 +16,7 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(helmet());
 app.use((req, res, next) => {
   req.user = {
     _id: "61027bfaec7a212ab4bba827",
@@ -27,12 +29,10 @@ app.use("/cards", require("./routes/cards"));
 
 app.use("*", (req, res) => {
   res
-    .status(404)
+    .status(400)
     .send(JSON.stringify({ message: "Запрашиваемый ресурс не найден" }));
 });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-  console.log("Ссылка на сервер");
-  console.log(BASE_PATH);
 });
