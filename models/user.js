3d4f2108fee9ea7300451065f80c89272958259b
Alexandr Mir <xelarim@ya.ru>
2021-08-11 15:30:51 +0300

    feat: default values && unrequred

--- models/user.js
+++ models/user.js
@@ -3,19 +3,20 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: "Жак-Ив Кусто",
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: "Исследователь",
   },
   avatar: {
     type: String,
-    required: true,
+    default:
+      "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
   },
   email: {
     type: String,
